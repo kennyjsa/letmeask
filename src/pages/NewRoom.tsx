c018dd3098c38a7c9fa26f2e7ff3c4d992d8e38c
Kenny Johnson <kenny.barao@gmail.com>
2021-08-04 07:25:19 -0400

    create pages home, newRoom

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,40 @@
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import googleIconImg from '../assets/images/google-icon.svg'
+
+import '../styles/auth.scss'
+import { Button } from '../components/Button'
+import { Link } from 'react-router-dom'
+
+export function NewRoom(){
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Aprenda e compartilhe conhecimento com outras pessoas</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          
+          <h2>
+            Crie uma nova sala
+          </h2>
+          <form>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+            />
+            <Button type="submit">Criar sala</Button>
+          </form>
+          <p>
+              Quer entrar em uma sala já existente?
+              <Link to="/">clique aqui</Link>
+            </p>
+        </div>
+      </main>
+
+    </div>
+  )
+}
