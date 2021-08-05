a18b6e9e11b336e0f7786051e70cd42e75a4d940
Kenny Johnson <kenny.barao@gmail.com>
2021-08-05 06:59:08 -0400

    hook useAuth

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,14 +1,15 @@
+import { Link } from 'react-router-dom'
+
+import { Button } from '../components/Button'
+import { useAuth } from '../hooks/useAuth'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 
 import '../styles/auth.scss'
-import { Button } from '../components/Button'
-import { Link } from 'react-router-dom'
-import { useContext } from 'react'
-import { AuthContext } from '../contexts/AuthContext'
 
 export function NewRoom(){
-  const { user } = useContext(AuthContext)
+  const { user } = useAuth()
 
   return (
     <div id="page-auth">
