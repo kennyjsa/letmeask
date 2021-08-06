856228e8ab709897e7b2aa7805f163a514ed7641
Kenny Johnson <kenny.barao@gmail.com>
2021-08-06 06:53:35 -0400

    home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,10 +10,15 @@ import googleIconImg from '../assets/images/google-icon.svg'
 
 
 import '../styles/auth.scss'
+import { FormEvent } from 'react'
+import { useState } from 'react'
+import { database } from '../services/firebase'
 
 export function Home() {
   const history = useHistory()
   const { user, signInWithGoogle } = useAuth()
+  const [roomCode, setRoomCode ] = useState('')
+
 
   async function handleCreateRoom() {
     if (!user) {
@@ -23,6 +28,22 @@ export function Home() {
     history.push("/rooms/new")
   }
 
+  async function handleJoinRoom(event:FormEvent) {
+    event.preventDefault()
+
+    if(roomCode.trim() === ''){
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get()
+    if(!roomRef.exists()){
+      alert('Room does not exists.')
+      return
+    }
+
+
+
+  }
 
   return (
     <div id="page-auth">
@@ -41,10 +62,13 @@ export function Home() {
           <div className="separator">
             ou entre em uma sala
           </div>
-          <form>
+          <form onSubmit={handleJoinRoom}>
             <input
               type="text"
               placeholder="Digite o código da sala"
+              
+              onChange={(event)=> setRoomCode(event.target.value)}
+              value={roomCode}
             />
             <Button type="submit">Entrar na sala</Button>
           </form>
