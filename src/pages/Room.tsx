849ad0e32e5d5612f57c1add24d57a966aa7329e
Kenny Johnson <kenny.barao@gmail.com>
2021-08-07 09:46:46 -0400

    include questions feature

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,23 +1,96 @@
-import { useState } from 'react'
+import { useEffect } from 'react'
+import { FormEvent, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import logoImg from '../assets/images/logo.svg'
 import { Button } from '../components/Button'
 import { RoomCode } from '../components/RoomCode'
+import { useAuth } from '../hooks/useAuth'
+import { database } from '../services/firebase'
 
 import '../styles/room.scss'
+
+type FirebaseQuestions = Record<string, {
+  author: {
+    name: string
+    avatar: string
+  }
+  content: string
+  isHighlighted: boolean
+  isAnswered: boolean
+}>
+
+type Question = {
+  id: string
+  author: {
+    name: string
+    avatar: string
+  }
+  content: string
+  isHighlighted: boolean
+  isAnswered: boolean
+}
 
 type RoomParams = {
   id: string
 }
 
-export function Room(){
+export function Room() {
+  const { user } = useAuth()
   const params = useParams<RoomParams>()
-  const roomID = params.id;
+  const roomID = params.id
 
+  const [title, setTitle] = useState('')
   const [newQuestion, setNewQuestion] = useState('')
+  const [questions, setQuestions] = useState<Question[]>([])
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomID}`)
 
-  async function handleSendQuestion(){
+    //TODO:TODAS AS INFORMACOES DE PERGUNTAS ESTÃO SENDO RECARREGADAS, ALTERAR PARA OUTROS EVENTOS
+    //ASSIM SERÁ MAIS PERFORMATICO
+    roomRef.on('value', room => {
 
+      const databaseRoom = room.val()
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {}
+
+      const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
+        return {
+          id: key,
+          content: value.content,
+          author: value.author,
+          isHighlighted: value.isHighlighted,
+          isAnswered: value.isAnswered,
+        }
+      })
+
+      setTitle(databaseRoom.title)
+      setQuestions(parsedQuestions)
+    })
+  }, [roomID])
+
+  async function handleSendQuestion(event: FormEvent) {
+    event.preventDefault()
+
+    if (newQuestion.trim() === '') {
+      return;
+    }
+
+    if (!user) {
+      throw new Error('You must be logged in')
+    }
+
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name,
+        avatar: user.avatar,
+      },
+      isHighlighted: false,
+      isAnswered: false
+    }
+
+    await database.ref(`rooms/${roomID}/questions`).push(question)
+
+    setNewQuestion('')
   }
 
   return (
@@ -31,22 +104,33 @@ export function Room(){
 
       <main className="content">
         <div className="room-title">
-          <h1>Sala React</h1>
-          <span>4 Perguntas</span>
+          <h1>Sala {title}</h1>
+          {questions.length > 0 && <span>{questions.length} perguntas</span>}
         </div>
 
-        <form>
-          <textarea 
-            placeholder="O que você quer perguntar?" 
-            onChange={(event)=>setNewQuestion(event.target.value)}
+        <form onSubmit={handleSendQuestion}>
+          <textarea
+            placeholder="O que você quer perguntar?"
+            onChange={(event) => setNewQuestion(event.target.value)}
             value={newQuestion}
           />
+
+          <div className="form-footer">
+            {user ? (
+              <div className="user-info">
+                <img src={user.avatar} alt={user.name} />
+                <span>{user.name}</span>
+              </div>
+            ) : (
+              <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+            )}
+            <Button type="submit">Enviar pergunta</Button>
+          </div>
         </form>
 
-        <div className="form-footer">
-          <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
-          <Button type="submit">Enviar pergunta</Button>
-        </div>
+        <pre>
+          {JSON.stringify(questions, null, 2)}
+        </pre>
       </main>
 
     </div>
